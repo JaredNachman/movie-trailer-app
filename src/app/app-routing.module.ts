2e46e3c54c1edd579418b7503b55fcd3dee5e27b
Jared Nachman <jnachman2020@chadwickschool.org>
2018-11-14 20:29:54 -0800

    Feat: add routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent }      from './app/app.component';
+import { ActorsComponent }      from './actors/actor.component';
+import { ActorDetailComponent } }      from './actor-detail/actor-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app', component: AppComponent },
+  { path: 'actors', component: ActorsComponent },
+  { path: 'actor-detail', component: ActorDetailComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
